3c7356ec7a62f0a346eeccd76edd4778f67a4f2b
iove <859321454@qq.com>
2024-06-04 17:24:44 +0800

    完成了学生管理系统java班初级版

--- test_1/project_1/src/Main.java
+++ test_1/project_1/src/Main.java
@@ -21,12 +21,34 @@ public class Main {
                     Add(list);
                 }
                 case 2->{
-
+                    System.out.println("请输入要删除的id:");
+                    String id=sc.next();
+                    if(Check(list,id)){
+                        list=Delete(list,id);
+                    }
+                    else {
+                        System.out.println("id不存在！退出操作");
+                    }
                 }
                 case 3->{
-
+                    System.out.println("请输入要修改的id:");
+                    String id=sc.next();
+                    if(Check(list,id)){
+                        list=Alter(list,id);
+                    }
+                    else {
+                        System.out.println("id不存在！退出操作");
+                    }
                 }
                 case 4->{
+                    System.out.println("请输入要查询的id:");
+                    String id=sc.next();
+                    if(Check(list,id)){
+                        int i=Find(list,id);
+                        stu s1=list.get(i);
+                        System.out.printf("%s\t%s\t%s\t%s",s1.getId(),s1.getName(),s1.getAge(),s1.getAddress());
+                        System.out.println();
+                    }
 
                 }
                 case 5->{
@@ -48,7 +70,7 @@ public class Main {
         System.out.println("请输入学生的id");
         String id=sc.next();
         boolean flag=Check(list,id);
-        if(flag==true){
+        if(flag){
             System.out.println("id已存在！退出操作");
             return;
         }
@@ -82,7 +104,64 @@ public class Main {
         }
         return -1;
     }
-    public static ArrayList<stu> Delete(ArrayList<stu> list,){
+    public static ArrayList<stu> Delete(ArrayList<stu> list,String id){
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i).getId().equals(id)){
+                list.remove(i);
+                System.out.println("删除成功！");
+                break;
+            }
+        }
+        return list;
+    }
+    public static ArrayList<stu> Alter(ArrayList<stu> list,String id){
+        Scanner sc=new Scanner(System.in);
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i).getId().equals(id)){
+                stu s1=list.get(i);
+                boolean temp=true;
+                System.out.println("请选择要修改的内容：");
+                while(temp){
+                    System.out.println("1：修改id");
+                    System.out.println("2：修改name");
+                    System.out.println("3：修改age");
+                    System.out.println("4：修改address");
+                    System.out.println("5：修改完成，退出进程");
+                    int n=sc.nextInt();
+                    switch (n){
+                        case 1->{
+                            System.out.println("请输入修改后的id:");
+                            String id2=sc.next();
+                            s1.setId(id2);
+                        }
+                        case 2->{
+                            System.out.println("请输入修改后的name:");
+                            String name=sc.next();
+                            s1.setName(name);
+                        }
+                        case 3->{
+                            System.out.println("请输入修改后的age:");
+                            int age=sc.nextInt();
+                            s1.setAge(age);
+                        }
+                        case 4->{
+                            System.out.println("请输入修改后的address:");
+                            String address=sc.next();
+                            s1.setAddress(address);
+                        }
+                        case 5->{
+                            temp=false;
+                        }
+                        default -> {
+                            System.out.println("输入错误！请重新输入");
+                        }
+                    }
 
+                }
+                list.set(i,s1);
+            }
+        }
+
+        return list;
     }
 }
